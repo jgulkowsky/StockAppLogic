ad9bacb03bd67de78dad12334f072cfc26fec356
jgulkowsky <jgulkowsky@gmail.com>
2024-01-06 19:38:48 +0100

    implement QuoteViewModel wrapper

--- Sources/StockAppLogicSwiftUI/ViewModels/QuoteViewModel.swift
+++ Sources/StockAppLogicSwiftUI/ViewModels/QuoteViewModel.swift
@@ -9,4 +9,61 @@ import Foundation
 import Combine
 import StockAppLogic
 
-public class QuoteViewModel: ObservableObject {}
+public class QuoteViewModel: ObservableObject {
+    @Published public var title: String = ""
+    @Published public var chartData: ChartData = ChartData(values: [])
+    @Published public var bidPrice: String = ""
+    @Published public var askPrice: String = ""
+    @Published public var lastPrice: String = ""
+    
+    private let viewModel: StockAppLogic.QuoteViewModel
+    private var store = Set<AnyCancellable>()
+    
+    public init(
+        coordinator: Coordinator,
+        quotesProvider: QuotesProviding,
+        chartDataProvider: ChartDataProviding,
+        symbol: String,
+        refreshRate: Double
+    ) {
+        self.viewModel = StockAppLogic.QuoteViewModel(
+            coordinator: coordinator,
+            quotesProvider: quotesProvider,
+            chartDataProvider: chartDataProvider,
+            symbol: symbol,
+            refreshRate: refreshRate
+        )
+        
+        self.viewModel.titlePublisher.sink { [weak self] value in
+            self?.title = value
+        }.store(in: &store)
+        
+        self.viewModel.chartDataPublisher.sink { [weak self] value in
+            self?.chartData = value
+        }.store(in: &store)
+        
+        self.viewModel.bidPricePublisher.sink { [weak self] value in
+            self?.bidPrice = value
+        }.store(in: &store)
+        
+        self.viewModel.askPricePublisher.sink { [weak self] value in
+            self?.askPrice = value
+        }.store(in: &store)
+        
+        self.viewModel.lastPricePublisher.sink { [weak self] value in
+            self?.lastPrice = value
+        }.store(in: &store)
+    }
+    
+    public func onViewWillAppear() {
+        viewModel.onViewWillAppear()
+    }
+
+    public func onViewWillDisappear() {
+        viewModel.onViewWillDisappear()
+    }
+    
+    public func onErrorRefreshButtonTapped() {
+        viewModel.onErrorRefreshButtonTapped()
+    }
+}
