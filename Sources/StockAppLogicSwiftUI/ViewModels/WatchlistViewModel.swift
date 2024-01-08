f1b136ff457e79698575885322381784dd62d798
jgulkowsky <jgulkowsky@gmail.com>
2024-01-08 11:21:24 +0100

    implement WatchlistViewModel wrapper

--- Sources/StockAppLogicSwiftUI/ViewModels/WatchlistViewModel.swift
+++ Sources/StockAppLogicSwiftUI/ViewModels/WatchlistViewModel.swift
@@ -9,4 +9,60 @@ import Foundation
 import Combine
 import StockAppLogic
 
-public class WatchlistViewModel: ObservableObject {}
+public class WatchlistViewModel: ObservableObject {
+    @Published public var title: String = ""
+    @Published public var stockItems: [StockItem] = []
+    
+    public var stockItemsCount: Int { viewModel.stockItemsCount }
+    
+    private let viewModel: StockAppLogic.WatchlistViewModel
+    private var store = Set<AnyCancellable>()
+    
+    public init(
+        coordinator: Coordinator,
+        watchlistsProvider: WatchlistsProviding,
+        quotesProvider: QuotesProviding,
+        watchlist: Watchlist,
+        refreshRate: Double
+    ) {
+        self.viewModel = StockAppLogic.WatchlistViewModel(
+            coordinator: coordinator,
+            watchlistsProvider: watchlistsProvider,
+            quotesProvider: quotesProvider,
+            watchlist: watchlist,
+            refreshRate: refreshRate
+        )
+        
+        self.viewModel.titlePublisher.sink { [weak self] value in
+            self?.title = value
+        }.store(in: &store)
+        
+        self.viewModel.stockItemsPublisher.sink { [weak self] value in
+            self?.stockItems = value
+        }.store(in: &store)
+    }
+    
+    public func onViewWillAppear() {
+        viewModel.onViewWillAppear()
+    }
+
+    public func onViewWillDisappear() {
+        viewModel.onViewWillDisappear()
+    }
+    
+    public func getStockItemFor(index: Int) -> StockItem? {
+        return viewModel.getStockItemFor(index: index)
+    }
+    
+    public func onItemTapped(at index: Int) {
+        viewModel.onItemTapped(at: index)
+    }
+    
+    public func onAddButtonTapped() {
+        viewModel.onAddButtonTapped()
+    }
+    
+    public func onItemSwipedOut(at index: Int) {
+        viewModel.onItemSwipedOut(at: index)
+    }
+}
