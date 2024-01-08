4edffa2108d3f29eef6dfc9cde6a09d00e8e4206
jgulkowsky <jgulkowsky@gmail.com>
2024-01-08 11:13:41 +0100

    implement AddNewWatchlistViewModel wrapper

--- Sources/StockAppLogicSwiftUI/ViewModels/AddNewWatchlistViewModel.swift
+++ Sources/StockAppLogicSwiftUI/ViewModels/AddNewWatchlistViewModel.swift
@@ -9,4 +9,40 @@ import Foundation
 import Combine
 import StockAppLogic
 
-public class AddNewWatchlistViewModel: ObservableObject {}
+public class AddNewWatchlistViewModel: ObservableObject {
+    @Published public var error: String?
+    @Published public var watchlistText: String = ""
+    
+    private let viewModel: StockAppLogic.AddNewWatchlistViewModel
+    private var store = Set<AnyCancellable>()
+    
+    public init(
+        coordinator: Coordinator,
+        watchlistsProvider: WatchlistsProviding,
+        emptyNameError: String = "Watchlist name can't be empty!",
+        watchlistAlreadyExistsError: String = "Watchlist with this name already exists!"
+    ) {
+        self.viewModel = StockAppLogic.AddNewWatchlistViewModel(
+            coordinator: coordinator,
+            watchlistsProvider: watchlistsProvider,
+            emptyNameError: emptyNameError,
+            watchlistAlreadyExistsError: watchlistAlreadyExistsError
+        )
+        
+        self.viewModel.errorPublisher.sink { [weak self] value in
+            self?.error = value
+        }.store(in: &store)
+        
+        self.viewModel.watchlistTextPublisher.sink { [weak self] value in
+            self?.watchlistText = value
+        }.store(in: &store)
+    }
+    
+    public func onTextFieldFocused(initialText: String?) {
+        viewModel.onTextFieldFocused(initialText: initialText)
+    }
+    
+    public func onTextFieldSubmitted(text: String?) {
+        viewModel.onTextFieldSubmitted(text: text)
+    }
+}
