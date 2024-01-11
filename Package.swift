cec2d90fbf5ab7373abbc3a4f210f70172493ae3
jgulkowsky <jgulkowsky@gmail.com>
2024-01-11 15:49:32 +0100

    copied CoreData Model from xcodeproj and added it into the Resource folder / removed support for macOS platform and added resources into StockAppLogic directed to the Resources folder / copied PersistenceController and WatchlistsCoreDataProvider into the package from the xcodeproj / made WatchlistsCoreDataProvider methods public and added public init

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "StockAppLogic",
     platforms: [
-        .macOS(.v12),
         .iOS(.v15),
     ],
     products: [
@@ -27,7 +26,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "StockAppLogic",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Resources")]),
         .target(
             name: "StockAppLogicSwiftUI",
             dependencies: ["StockAppLogic"]),
